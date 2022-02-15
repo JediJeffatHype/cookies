d0d6d6531c85d3c082b622cbe7c86d0f24832c4d
JediJeffatHype <75488088+JediJeffatHype@users.noreply.github.com>
2022-02-15 12:50:24 -0700

    Create cookies.js

---
+++ cookies.js
@@ -0,0 +1,5 @@
+module.exports = function cookies(input) { //open code
+    if (typeof input == "number") console.log('you put in a number') //detects if its an integer/number
+    if (typeof input == "boolean") console.log('true/false statement') //detects if it is a boolean
+    if (typeof input == "string") console.log('string!') //detects if it is a string
+} //end code
